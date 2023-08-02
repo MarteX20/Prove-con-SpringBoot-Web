0d3b810bcca324de574c271d1c4d181574f819b8
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-08-02 23:20:15 +0200

    salvataggio di 2 studenti per fare prove GET

--- src/main/java/SpringBlack/black/student/StudentConfig.java
+++ src/main/java/SpringBlack/black/student/StudentConfig.java
@@ -1,4 +1,27 @@
 package SpringBlack.black.student;
 
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
 public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return  args -> {
+            Student sve = new Student("Sveta", "sve@gmail.com", LocalDate.of(2000, APRIL, 1), 23);
+
+            Student max = new Student("Maksym", "maksym@gmail.com", LocalDate.of(2000, APRIL, 15), 23);
+
+            repository.saveAll(List.of(sve, max));
+        };
+    }
 }
